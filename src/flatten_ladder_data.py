21e59ac9c2896be18f7bd1f48a7d36859f75a82f
krterberg <github@terberg.dev>
2022-08-11 19:45:29 +0200

    Add skill modes and ladder position Track the output in git LFS

--- src/flatten_ladder_data.py
+++ src/flatten_ladder_data.py
@@ -10,6 +10,9 @@ def process_league_day(league_day: dict, league_label, day_label) -> pd.DataFram
     skill_users = league_day['activeSkillUse']
     user_levels = league_day['levels']
     user_names = league_day['names']
+    user_ranks = league_day['ladderRanks']
+    user_skill_modes = league_day['skillModeUse']
+    skill_modes = league_day['skillModes']
 
     # We'd prefer to use the dpsName (vaal and alternate qualities merged together)
     # but that causes problems with our dictionary keys
@@ -20,22 +23,35 @@ def process_league_day(league_day: dict, league_label, day_label) -> pd.DataFram
     skill_users = {key:[user_names[value] for value in values] for (key, values) in skill_users.items()}
     skill_users = pd.DataFrame.from_dict(skill_users, orient='index').stack().reset_index()
     skill_users.columns = ['skill_name', '_', 'character_name']
-    characters_levels = pd.DataFrame.from_dict(zip(user_names, user_levels))
-    characters_levels.columns = ['character_name', 'character_level']
+    
+    user_skill_modes = {skill_modes[int(key)]['name']:values for (key, values) in user_skill_modes.items()}
+    user_skill_modes = {key:np.cumsum(values) for (key, values) in user_skill_modes.items()}
+    user_skill_modes = {key:[user_names[value] for value in values] for (key, values) in user_skill_modes.items()}
+    user_skill_modes = pd.DataFrame.from_dict(user_skill_modes, orient='index').stack().reset_index()
+    user_skill_modes.columns = ['skill_mode', '_', 'character_name']
+    # users can have more than one skill mode, so let's one-hot encode it
+    user_skill_modes = (user_skill_modes
+        .assign(_ = 1)
+        .pivot_table(columns='skill_mode', index='character_name', values='_', aggfunc=np.max)
+        )
 
-    characters_skills_level = skill_users.merge(characters_levels, on='character_name')
+    characters_levels_ranks = pd.DataFrame.from_dict(zip(user_names, user_levels, user_ranks))
+    characters_levels_ranks.columns = ['character_name', 'character_level', 'ladder rank']
+
+    characters_skills_level = skill_users.merge(characters_levels_ranks, on='character_name')
+    characters_skills_level = skill_users.merge(user_skill_modes, on='character_name')
     characters_skills_level['league'] = league_label
     characters_skills_level['day'] = day_label
     return characters_skills_level
 
 
 league_days = []
-for league_label in sentinel_leagues.keys():
-    league = sentinel_leagues[league_label]
-    for day_label in league.keys():
-        day = league[day_label]
+for league_name in sentinel_leagues.keys():
+    league_data = sentinel_leagues[league_name]
+    for day_name in league_data.keys():
+        day_data = league_data[day_name]
         league_days.append(
-            process_league_day(day, league_label, day_label)
+            process_league_day(day_data, league_name, day_name)
         )
 
 league_days = pd.concat(league_days)
